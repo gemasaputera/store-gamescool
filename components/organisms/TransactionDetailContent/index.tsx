28c4ea28d40785cf2a6962239a3f064debc888e4
gemasaputera <gema.saputera@gmail.com>
2022-08-12 21:38:23 +0700

    feat: transaction detail and add example storybook

--- components/organisms/TransactionDetailContent/index.tsx
+++ components/organisms/TransactionDetailContent/index.tsx
@@ -45,50 +45,21 @@ const TransactionDetailContent = () => (
               <Row label="Item" value="250 Diamonds" />
               <Row label="Price" value="Rp 42.280.500" />
               <Row label="Tax (10%)" value="Rp 4.228.000" />
-              <Row label="Total" value="Rp 55.000.600" />
-              <p className="text-lg color-palette-1 mb-20">
-                Order ID<span className="purchase-details">#GG001</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Item<span className="purchase-details">250 Diamonds</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Price<span className="purchase-details">Rp 42.280.500</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Tax (10%)
-                <span className="purchase-details">Rp 4.228.000</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Total
-                <span className="purchase-details color-palette-4">
-                  Rp 55.000.600
-                </span>
-              </p>
+              <Row
+                label="Total"
+                customClass="color-palette-4"
+                value="Rp 55.000.600"
+              />
             </div>
             <div className="payment pt-10 pb-10">
               <h2 className="fw-bold text-xl color-palette-1 mb-20">
                 Payment Informations
               </h2>
-              <p className="text-lg color-palette-1 mb-20">
-                Your Account Name
-                <span className="purchase-details">Masayoshi Angga Zero</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Type
-                <span className="payment-details">Worldwide Transfer</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Bank Name<span className="payment-details">Mandiri</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Bank Account Name
-                <span className="payment-details">PT Store GG Indonesia</span>
-              </p>
-              <p className="text-lg color-palette-1 mb-20">
-                Bank Number
-                <span className="payment-details">1800 - 9090 - 2021</span>
-              </p>
+              <Row label="Your Account Name" value="Masayoshi Angga Zero" />
+              <Row label="Type" value="Worldwide Transfer" />
+              <Row label="Bank Name" value="Mandiri" />
+              <Row label="Bank Account Name" value="PT Store GG Indonesia" />
+              <Row label="Bank Number" value="1800 - 9090 - 2021" />
             </div>
             <div className="d-md-block d-flex flex-column w-100">
               <a
